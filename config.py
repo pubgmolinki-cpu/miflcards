5dc4b082a8eaad5ea9a514a0abd33a553d865852
pubgmolinki-cpu <pubgmolinki@gmail.com>
2026-05-10 10:50:06 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+AI_API_KEY = os.getenv("AI_API_KEY") # Ключ для нейросети (Gemini/OpenAI)
 CHANNEL_ID = "@Miflcards"
-
 ADMIN_IDS = [1866813859]
+
+RARITY_BOOSTS = {
+    "Stock": 0.05,
+    "Series": 0.15,
+    "Drop": 0.30,
+    "Chase": 0.50,
+    "One": 1.00
+}
+
+RARITY_RANKS = {"Stock": 1, "Series": 2, "Drop": 3, "Chase": 4, "One": 5}
